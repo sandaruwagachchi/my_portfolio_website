2107edf6a69cb7c62904cd8d77c06198bb1269bb
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2024-12-30 12:25:40 +0530

    updated netflix image

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import styles from './ProjectsStyles.module.css';
 import movie from '../../assets/assets/movie.webp';
-import netflix from '../../assets/assets/movie.webp';
+import netflix from '../../assets/assets/netflix.webp';
 import news from '../../assets/assets/news.webp';
 import gemini from '../../assets/assets/gemini.webp';
 import hotel from '../../assets/assets/hotel.webp';
