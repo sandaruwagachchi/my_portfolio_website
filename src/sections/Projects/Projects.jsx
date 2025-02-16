e6b1da5f2e189775c6308b9100b0646141f90a39
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2025-02-16 09:05:43 +0530

    add new projectCard
    Apple iPhone 15 Pro Website

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import news from '../../assets/assets/news.webp';
 import gemini from '../../assets/assets/gemini.webp';
 import hotel from '../../assets/assets/hotel.jpg';
 import ProjectCard from '../../common/ProjectCard';
+import iPhone from '../../assets/assets/iPhone.jpg';
 
 function Projects() {
   return (
@@ -15,31 +16,37 @@ function Projects() {
           src={movie}
           link="https://github.com/sandaruwagachchi/Movie-Tickets-Booking-System"
           h3="Movie Tickets Booking System"
-          p="Using Java Language"
+          p="Using Java Language and MySQL"
         />
         <ProjectCard
           src={hotel}
           link="https://github.com/sandaruwagachchi/Hotel-Management-System"
           h3="Hotel Management System"
-          p="Using C# Language"
+          p="Using C# Language and MySQL"
         />
          <ProjectCard
           src={netflix}
           link="https://github.com/sandaruwagachchi/Netflix-Clone"
-          h3="Netflix Clone"
+          h3="Netflix Clone Website"
           p="Using React JS and Firebase"
         />
          <ProjectCard
           src={news}
           link="https://github.com/sandaruwagachchi/news-website"
-          h3="SandaNews-360"
+          h3="SandaNews-360 Website"
           p="Using React JS"
         />
          <ProjectCard
           src={gemini}
           link="https://github.com/sandaruwagachchi/gemini-clone"
-          h3="Gemini Clone"
+          h3="Gemini Clone Website"
           p="Using React JS"
+        />
+         <ProjectCard
+          src={iPhone}
+          link="https://github.com/sandaruwagachchi/apple-clone"
+          h3="Apple iPhone 15 Pro Website"
+          p="Using React JS, GSAP, Three JS"
         />
       </div>
     </section>
